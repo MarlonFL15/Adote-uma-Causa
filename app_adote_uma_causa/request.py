7dc61aec70120053f38cd4cfd8afec01c72ff9c7
unknown <marlonfleite50@gmail.com>
2021-10-05 08:27:27 -0400

    integrando a página home com a api

---
+++ app_adote_uma_causa/request.py
@@ -0,0 +1,14 @@
+#Caso queira baixar e utilizar o projeto, acesse o site da globalgiving API e insira sua chave privada nessa variável
+import requests
+
+API_KEY = 'your_key'
+
+headers = {
+    'accept': 'application/json',
+    'content-Type': 'application/json'
+}
+
+def make_request(url):
+    response = requests.get(url+'?api_key='+API_KEY, headers=headers)
+    return response.json()
+
--- app_adote_uma_causa/views.py
+++ app_adote_uma_causa/views.py
@@ -1,11 +1,42 @@
 from django.shortcuts import render
 
+from django.http import JsonResponse
+from .request import make_request
+
+themes = {
+    'hunger':'Comida saudável',
+    'water': 'Água potável',
+    'health': 'Assistência médica',
+    'housing': 'Acesso à moradia',
+    'edu': 'Educação',
+    'children': 'Proteção infantil',
+    'animals': 'Bem-estar animal',
+    'climate': 'Ações climáticas',
+    'gender': 'Igualdade de gênero',
+    'rights': 'Justiça e direitos humanos',
+    'Direitos LGBTQIA+': 'lgbt',
+    'Igualdade Racial': 'justice'
+}
+
 # Create your views here.
 def home(request):
-    return render(request, 'app_adote_uma_causa/home.html')
+    url = 'https://api.globalgiving.org/api/public/projectservice/featured/projects'
+    json = make_request(url)
+    projects = []
+
+    for j in json['projects']['project']:
+        projects.append({
+            'title': j['title'],
+            'summary': j['summary'],
+            'funding': j['funding'],
+            'goal': j['goal'],
+            'percent': float(j['funding']) / float(j['goal']) * 100,
+            'image_link': j['image']['imagelink'][3]['url']
+        })
+    return render(request, 'app_adote_uma_causa/home.html', {'projects':projects})
 
 def projects(request):
-    return render(request, 'app_adote_uma_causa/projects.html')
+    return render(request, 'app_adote_uma_causa/projects.html', {'themes': themes})
 
 def contact(request):
     return render(request, 'app_adote_uma_causa/contact.html')
@@ -15,3 +46,11 @@ def about(request):
 
 def base(request):
     return render(request, 'app_adote_uma_causa/base.html')
+
+def teste(request):
+    url = 'https://api.globalgiving.org/api/public/projectservice/featured/projects'
+    json = make_request(url)
+    projects = []
+
+
+    return JsonResponse(json, safe=False)
